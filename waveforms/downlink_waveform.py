3233b15eee8987771765391b8ba267409311ed19
Siddhant Raman <sraman.iisc@gmail.com>
2018-09-19 02:06:51 -0700

    [DL Waveform] Adds function : ofdm_dl_waveform_5g
    ofdm_dl_waveform_5g() generates N symbols of 5G waveform, given mu, Nrb
    and Num of Guard Tones on each side of spectrum
    The BPSK/QPSK symbols are taken from modData.txt file.

--- waveforms/downlink_waveform.py
+++ waveforms/downlink_waveform.py
@@ -53,7 +53,13 @@ def cp_ofdm_waveform(mu=0, Nrb=20, Guard=7):
     plt.show()
     print(0) 
 
-def ofdm_dl_waveform_5g(mu=0, Nrb=20, Guard=7):
+#################################################################################################################
+# Function to generate NumOfSymbols OFDM symbol based on waveform specified in 5G
+# input params : NumOfSymbols, numerology(mu), Nrb (Resource Blocks), Guard tones on both side of spectrum
+# output : NumOfSymbols 5G OFDM symbols with Cyclic Prefix of 288 samples
+# Algorithm is simple to understand from the code
+#################################################################################################################
+def ofdm_dl_waveform_5g(NumOfSymbols=14, mu=0, Nrb=20, Guard=7):
     #########################################
     NFFT = 4096
     NoOfCarriers = 12*Nrb
@@ -63,42 +69,60 @@ def ofdm_dl_waveform_5g(mu=0, Nrb=20, Guard=7):
     subFrameDur = 1/1000 # 1msec
     FrameDur = 10*subFrameDur #10 msec
     T_symbol = 1/deltaF
-    deltaF_max = deltaF * 2**5 # 480KHz
-    deltaF_ref = deltaF
+    deltaF_max = 15000 * 2**5 # 480KHz
+    deltaF_ref = 15000
     NFFT_ref = 2048
     Bandwidth = deltaF * NoOfCarriers
     ##########################################
     #Derived Params
-    kappa = deltaF_max*NFFT/deltaF_ref/NFFT_ref
-    Nu = NFFT_ref * kappa /2**mu #Num of OFDM samples in 32 symbols
-    Ncpmu = 144*kappa/2**mu # total CP samples in 32 symbols
+    kappa = (deltaF_max*NFFT)/(deltaF_ref*NFFT_ref)
+    Nu = NFFT_ref * kappa /(2**mu) #Num of OFDM samples in 32 symbols
+    Ncpmu = 144*kappa/(2**mu) # total CP samples in 32 symbols
     Ts = 1/deltaF_max/NFFT # symbolDur if Symbol occupies 480*4096 KHz (Max bandwidth possible in 5G NR with 4096 tones)
     ###########################################
 
-    t = np.linspace(0, (Nu+Ncpmu)*Ts, NFFT + CPSize)
+    t = np.linspace(0, (Nu+Ncpmu)*Ts, NFFT + CPSize, endpoint=False)
     data = np.loadtxt('modData.txt')
     NumData = NoOfCarriers - 2*Guard - 1
-    a_k = np.concatenate((np.zeros(Guard), data[:int((NumData+1)/2)], np.zeros(1)))
-    a_k = np.concatenate((a_k, data[int((NumData+1)/2):int(NumData)], np.zeros(Guard)))
-    k = np.linspace(- int(NoOfCarriers/2), int(NoOfCarriers/2)-1, NoOfCarriers)
-    print(t.size)
-    S_t = np.zeros(t.size)
-    for i in k:
-        S_t = a_k[int(i)] * np.exp(1j*2*np.pi*i*deltaF*(t - Ncpmu*Ts)) + S_t
+
+    x = 113*np.pi/60 * np.sinc(113/120 * np.linspace(-255, 255, 511))
+    x_win = x * (0.5*(1 + np.cos(2*np.pi/510 * np.linspace(-255, 255, 511))))**0.6
+    for num in range(0, NumOfSymbols):
+        a_k = np.concatenate((np.zeros(Guard), data[NumData*num : NumData*num + int((NumData+1)/2)], np.zeros(1)))
+        a_k = np.concatenate((a_k, data[NumData*num + int((NumData+1)/2):int(NumData) + NumData*num], np.zeros(Guard)))
+        k = np.linspace(- int(NoOfCarriers/2), int(NoOfCarriers/2)-1, NoOfCarriers)
+        S_t = np.zeros(t.size)
+        for i in k:
+            S_t = a_k[int(i+NoOfCarriers/2)] * np.exp(1j*2*np.pi*i*deltaF*(t - Ncpmu*Ts)) + S_t
+        #S_t = np.convolve(S_t, x_win, mode='same')/sum(x_win)
+        if(num == 0):
+            S = S_t
+        else:
+            S = np.concatenate((S, S_t))
+            
+    #print(S.size, Nu, Ncpmu)
+    #print(deltaF, Ts)
+    print(a_k[120:130])
+    x1 = np.fft.fft(S[-4096:], 4096)/4096
+    print(np.real(x1[:10]))
+    #fig2 = plt.figure()
+    #plt.stem(np.linspace(0, 4096, 4096), x1, '-r')
+
+    #fig1 = plt.figure()
+    CPdur = (CPSize/NFFT) * (1/deltaF)
+    Fs = (NFFT+CPSize)/(T_symbol + CPdur)
+    #print(Fs, Bandwidth, NoOfCarriers)
+    #print(CPdur, T_symbol)
+    #f, Pxx_den = signal.periodogram(S, Fs, return_onesided=False)
+    #plt.ylim((10**-10, 2*np.max(Pxx_den)))
+    #plt.semilogy(f, Pxx_den, '-g')
+    #f, Pxx_den = signal.welch(S, Fs, return_onesided=False)
+    #plt.semilogy(f, Pxx_den, '-r')
+    #plt.xlim((-Bandwidth, Bandwidth))
 
     fig1 = plt.figure()
-    plt.stem(t, S_t, '-g')
-
-    fig2 = plt.figure()
-    CPdur = CPSize/NFFT * 1/deltaF
-    Fs = (NFFT+CPSize)/(T_symbol + CPdur)
-    print(Fs)
-    f, Pxx_den = signal.periodogram(S_t, Fs)
-    plt.semilogy(f, Pxx_den, '-g')
-    f, Pxx_den = signal.welch(S_t, Fs)
-    plt.semilogy(f, Pxx_den, '-r')
-    plt.xlim((-(Bandwidth+2*10**6)/2, (Bandwidth+2 * 10**6)/2))
-    plt.ylim((10**-10, 2*np.max(Pxx_den)))
+    plt.psd(S, 4096, Fs)
+    plt.xlim((-Nrb*7.5*deltaF, Nrb*7.5*deltaF))
     plt.show()
 
-ofdm_dl_waveform_5g(0, 20, 7)
+ofdm_dl_waveform_5g(14, 0, 20, 7)
