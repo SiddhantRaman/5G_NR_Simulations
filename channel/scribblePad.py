445bd08d40e777bf7562a8f0999337d744b11aa9
Siddhant Raman <sraman.iisc@gmail.com>
2018-09-20 02:10:21 -0700

    [Channel Module] All deformities Simulated
    scribblePad.py : To do all the rough work on Communication channel

---
+++ channel/scribblePad.py
@@ -0,0 +1,9 @@
+##############################################################################
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+##############################################################################
+
+def awgn_noise(length, power):
+    print("TODO : AWGN NOISE: power and length")
