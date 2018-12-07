cb999f4ecd4b76b7d02c80d36fd81d03d14106b6
Siddhant Raman <sraman.iisc@gmail.com>
2018-12-08 02:43:52 +0530

    [Generates LTE CellSpecific Reference Sequence]
    Gold Code used to generate Pseudo Random Sequence for CRS.

--- channel/scribblePad.py
+++ channel/scribblePad.py
@@ -31,7 +31,7 @@ def window_lpf_fir(wc=0.5*np.pi, window='Cos', numTaps=255):
     return firFilt
 
 def window_bpf_fir(w1=0.25*np.pi, w2=0.5*np.pi, window='Cos', numTaps=255):
-    filt = (w2/np.pi) * np.sinc((w2/np.pi) * np.linspace(-(numTaps-1/2), (numTaps-1)/2, numTaps)) - (w1/np.pi) * np.sinc((w1/np.pi) * np.linspace(-(numTaps-1)/2, (numTaps-1)/2, numTaps))
+    filt = (w2/np.pi) * np.sinc((w2/np.pi) * np.linspace(-(numTaps-1)/2, (numTaps-1)/2, numTaps)) - (w1/np.pi) * np.sinc((w1/np.pi) * np.linspace(-(numTaps-1)/2, (numTaps-1)/2, numTaps))
     if window == 'Cos':
         filt_win = np.cos(np.linspace(0, np.pi/2, 16))**2
         firFilt = np.concatenate((filt[:16]*np.flip(filt_win), filt[16:-16], filt[-16:]*filt_win))
@@ -39,15 +39,39 @@ def window_bpf_fir(w1=0.25*np.pi, w2=0.5*np.pi, window='Cos', numTaps=255):
         firFilt = filt
     return firFilt
 
+def delta_func(length=4096):
+    Ts = 1/(12*20*15000)
+    t = np.linspace(0, Ts, length, endpoint=False)
+    delta = np.concatenate((np.zeros(10), np.ones(1), np.zeros(length-11)))
+    return delta
+
+# Generate AWGN noise of Power -130dBm/Hz and BW 3.6MHz
 noise = awgn_noise(300000, -130, 12*20*15000)
 fig1 = plt.figure()
+
+# Plot Noise PSD in Blue
 plt.psd(noise, 4096, 12*20*15000)
-firFilt = window_lpf_fir(0.45*np.pi, 'Cos', 255)
-freq1, resp1 = signal.freqz(firFilt)
-print(firFilt.size)
-#plt.plot(freq1, 20*np.log10(abs(resp1)), '-r')
-#plt.plot(freq2, 20*np.log10(abs(resp2)), '-b')
-#plt.show()
+
+# Generate 255 Tap FIR BPF from pi/4 till 3pi/4 with rectangular window in Red
+firFilt = window_bpf_fir(0.25*np.pi, 0.75*np.pi, 'None', 255)
+# Filter the Noise with this filter
 filtered_noise = np.convolve(noise, firFilt, mode='same')
 plt.psd(filtered_noise, 4096, 3.6*10**6)
+
+# Generate 255 Taps FIR BPF from pi/4 till 3pi/4 with Cos^2 Window in Green
+firFilt = window_bpf_fir(0.25*np.pi, 0.75*np.pi, 'Cos', 255)
+# Filter the Noise with this filter
+filtered_noise = np.convolve(noise, firFilt, mode='same')
+plt.psd(filtered_noise, 4096, 3.6*10**6)
+
+# Plot the Un-Filtered Noise and Filtered Noise
+plt.title('FIR Filter : Rect Window Vs Cos^2 Window')
+
+# Plot the spectrum of a delta function : 
+#    0dBm/Hz constant for all frequencies
+fig2 = plt.figure()
+d = delta_func(4096)
+print(d.size, d[:10])
+plt.plot(np.linspace(0, 4095, 4096), 20*np.log10(np.abs(np.fft.fft(d))) ,'-r')
+plt.plot(np.linspace(0, 4095, 4096), np.imag(np.fft.fft(d))/np.real(np.fft.fft(d)), '-g')
 plt.show()
---
+++ waveforms/gen_lte_crs.py
@@ -0,0 +1,47 @@
+# Generate DL Reference Sequences
+import numpy as np
+import matplotlib.pyplot as plt
+# Generate Cell Specific Reference Sequence
+# rl,ns(m) = 1/sqrt(2).(1 - 2.c(2m)) +j1/sqrt(2).(1 - 2.c(2m+1)), m = 0,1,2,3,...,2Nrb, max - 1
+# c(n) = (x(n+Nc) + y(n+Nc))mod2
+# x(n+31) = (x(n+3) + x(n))mod 2
+# y(n+31) = (y(n+3) + y(n+2) + y(n+1) + y(n))mod 2
+# c_init and Length of the psedo-random sequence (c(n)) needs to be specified
+# inputs : Ns : Slot number within a Frame
+#          L  : Lth OFDM Symbol within the slot
+#          N_CellID : Physical Layer Cell ID
+# output : crs_lte : Cell Specific Reference Sequence
+
+SQRT_2 = np.sqrt(2)
+
+def gen_lte_crs(Ns, L, N_CellID):
+    N_cp = 1
+    N_rb_dlmax = 110
+    c_init = 1024*(7*(Ns +1) + L + 1)*(2*N_CellID + 1) + 2*N_CellID + N_cp
+    # Generate Gold Sequence of len = 2*N_rb_dlmax
+    length = 2 * N_rb_dlmax
+    x = np.zeros(1600 + length)
+    y = np.zeros(1600 + length)
+    c_init_tmp = c_init
+    for n in range(0,31):
+        y[30-n+1] = np.floor(c_init_tmp/np.power(2,(30-n)))
+        c_init_tmp = c_init_tmp - np.floor(c_init_tmp/np.power(2,(30-n))) * np.power(2,(30-n))
+    x[1] = 1
+
+    for n in range(0, 1600+length-32):
+        x[n+31+1] = np.mod((x[n+3+1] + x[n+1]), 2)
+        y[n+31+1] = np.mod((y[n+3+1] + y[n+2+1] + y[n+1+1] + y[n+1]), 2)
+    c = np.zeros(length)
+    for n in range(1, length):
+        c[n] = np.mod((x[n+1600] + y[n+1600]), 2)
+
+    crs = np.zeros(N_rb_dlmax) + 1j*np.zeros(N_rb_dlmax)
+    for n in range(0, N_rb_dlmax):
+        crs[n] = 1/SQRT_2 * (1 - 2*c[2*n]) + 1j*(1/SQRT_2 * ( 1 - 2*c[2*n + 1]))
+    return crs
+
+crs = gen_lte_crs(4, 4, 10)
+print(len(crs))
+print(crs)
+plt.scatter(np.real(crs), np.imag(crs))
+plt.show()
